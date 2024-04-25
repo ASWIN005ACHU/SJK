db1b558906f4703ba56b9af5196bb3b3bb57c6d9
aswin005achu <aswin005achu@gmail.com>
2024-04-25 14:55:09 +0000

    Project Started

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import './App.css'
+import Logo from '../public/1.svg'
+import { BrowserRouter } from 'react-router-dom'
+
+
+function App() {
+
+  return (
+    <BrowserRouter>
+      <h1 className='text-center'>Hello World!</h1>
+    </BrowserRouter>
+  )
+}
+
+export default App
